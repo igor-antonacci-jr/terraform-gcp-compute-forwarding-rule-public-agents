c3afdfa74001a354cc49ef34398d1f1e7bbdd4cb
Jan Ulferts <julferts@mesosphere.com>
2018-09-21 16:27:55 +0200

    remove to broad port range

--- main.tf
+++ main.tf
@@ -20,21 +20,6 @@
 
 provider "google" {}
 
-# TODO: do we have an official doc for this? It makes sense from edgeLB
-#       perspective but I miss an doc for this
-locals {
-  default_rules = [{
-    port_range = "1-21"
-  },
-    {
-      port_range = "23-5050"
-    },
-    {
-      port_range = "5052-32000"
-    },
-  ]
-}
-
 module "dcos-forwarding-rule-public-agents" {
   source  = "dcos-terraform/compute-forwarding-rule/gcp"
   version = "~> 0.0"
@@ -44,7 +29,7 @@ module "dcos-forwarding-rule-public-agents" {
   instances_self_link = ["${var.public_agents_self_link}"]
   name_format         = "${var.name_format}"
 
-  rules = ["${concat(local.default_rules,var.additional_rules)}"]
+  additional_rules = ["${var.additional_rules}"]
 
   health_check {
     target = "/_haproxy_health_check"
